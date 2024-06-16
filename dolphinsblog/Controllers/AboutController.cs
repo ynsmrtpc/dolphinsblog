72352127b481b1dde849c2926b3e5b9ab7cc2fc5
ynsmrtpc <yunusemre_topcu@hotmail.com>
2024-06-16 23:42:16 +0300

    added blog partials and start adding blogdetails partials

---
+++ dolphinsblog/Controllers/AboutController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dolphinsblog.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult Footer()
+        {
+            ViewData["CurrentYear"] = 2024;
+            return PartialView();
+        }
+    }
+}
---
+++ dolphinsblog/Controllers/BlogController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dolphinsblog.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult BlogList() 
+        {
+            return PartialView();
+        }
+        
+        public PartialViewResult FeaturedPost()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult OtherFeaturedPost()
+        {
+            return PartialView();
+        }
+        public PartialViewResult MailSubscribe()
+        {
+            return PartialView();
+        }
+
+        public IActionResult BlogDetails()
+        {
+            return View();
+        }
+        public PartialViewResult BlogCover()
+        {
+            return PartialView();
+        }
+    }
+}
